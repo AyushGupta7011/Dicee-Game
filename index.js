b8d80835796618557a3c3dc3d2636499c2c89746
Ayush <guptaayush630@gmail.com>
2020-07-23 22:53:48 +0530

    Adding Dice Images to Game

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
